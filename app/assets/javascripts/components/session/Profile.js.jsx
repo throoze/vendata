b242fa15d84c0265f9ceac1962dd1d7fdd61c99c
Victor De Ponte <rdbvictor19@gmail.com>
2016-01-12 21:49:39 -0430

    style problems fixed

--- app/assets/javascripts/components/session/Profile.js.jsx
+++ app/assets/javascripts/components/session/Profile.js.jsx
@@ -1,52 +1,46 @@
-var React 				  = require('react');
-
-var SessionStore          = require('../../stores/SessionStore.js.jsx');
+var React         = require('react');
+var SessionStore  = require('../../stores/SessionStore.js.jsx');
 var NavigationBar = require('../../components/layouts/NavigationBar.js.jsx');
-
-var BS                    = require('react-bootstrap');
-var Input                 = BS.Input,
-	ButtonInput           = BS.ButtonInput;
+var BS            = require('react-bootstrap');
+var Input         = BS.Input,
+    ButtonInput   = BS.ButtonInput;
 
 
 function getStateFromStores() {
-  return {
-    isLoggedIn: SessionStore.isLoggedIn(),
-    user: JSON.parse(SessionStore.getUser()),
-    email:SessionStore.getEmail()
-  };
+    return {
+        isLoggedIn: SessionStore.isLoggedIn(),
+        user: JSON.parse(SessionStore.getUser()),
+        email:SessionStore.getEmail()
+    };
 }
 
 
 Profile = React.createClass({
 
-	getInitialState: function() {
-    	return getStateFromStores();
-  	},
+    getInitialState: function() {
+        return getStateFromStores();
+    },
 
-  	componentDidMount: function() {
-   		SessionStore.addChangeListener(this._onChange);
- 	},
+    componentDidMount: function() {
+        SessionStore.addChangeListener(this._onChange);
+    },
 
-  	componentWillUnmount: function() {
-    	SessionStore.removeChangeListener(this._onChange);
-  	},
+    componentWillUnmount: function() {
+        SessionStore.removeChangeListener(this._onChange);
+    },
 
-  	_onChange: function() {
-    	this.setState(getStateFromStores());
-  	},
+    _onChange: function() {
+        this.setState(getStateFromStores());
+    },
 
-	render: function() {
-		var mensaje = "Prueba Profile";
-    	return (
-    		<div className="profile-page">
-    			//<NavigationBar isLoggedIn={this.state.isLoggedIn} email={this.state.email} />
-
-    			//Imagen de profile
-    			{mensaje}
-    		</div>
-    	);
-  	}
-
+    render: function() {
+        var mensaje = "Prueba Profile";
+        return (
+            <div className="profile-page">
+                {mensaje}
+            </div>
+        );
+    }
 });
 
 module.exports = Profile;
