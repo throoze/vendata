91b75443a283709a41e20bb978258e047462a10c
Francisco Martinez <frammnm@gmail.com>
2016-01-28 23:06:10 -0430

    Arreglago link de perfil

--- app/assets/javascripts/components/layouts/NavigationBar.js.jsx
+++ app/assets/javascripts/components/layouts/NavigationBar.js.jsx
@@ -72,17 +72,17 @@ NavigationBar = React.createClass({
     render: function() {
         var rightItem = this.props.isLoggedIn ? (
             <NavDropdown eventKey={4} title={this.props.email} id="collapsible-nav-dropdown">
-                <MenuItem eventKey={1}>Perfil</MenuItem>
+                <MenuItem eventKey={1}><Link to="profile">Perfil</Link></MenuItem>
                 <MenuItem eventKey={2}><Link to="scraping">Scraping</Link></MenuItem>
                 <MenuItem divider />
                 <MenuItem eventKey={3}>
-                    <Button  onClick={this._handleLogout} bsStyle="primary" bsSize="small">Logout</Button>
+                    <Button  onClick={this._handleLogout} bsStyle="primary" bsSize="small">Salir</Button>
                 </MenuItem>
             </NavDropdown>
             ) : (
             <Dropdown className="login-button" open={this.state.open} id="dropdown" >
               <DropdownToggle bsRole="toggle" onClick={this._setOpen}>
-                Login
+                Entrar
               </DropdownToggle>
 
               <DropdownMenu  className="login-form" bsRole="menu">
