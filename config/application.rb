878a68b97f01791efdc299c4a02fdb1ded33a20c
Victor De Ponte <rdbvictor19@gmail.com>
2016-01-28 19:49:12 -0430

    Env vars load moved to application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
+# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
+app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
+load(app_environment_variables) if File.exists?(app_environment_variables)
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
